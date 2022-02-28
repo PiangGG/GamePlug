990daa62d7fa67ee099702b9f022def1cb950986
PiangGG <974805475@qq.com>
2022-02-28 10:49:05 +0800

    init project

---
+++ Source/GamePlug/GamePlugGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GamePlugGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPLUG_API AGamePlugGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
